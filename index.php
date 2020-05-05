7a7ff78dba6f52f965b83b44c8a3dc1d5a4bae44
narges <nargestv@gmail.com>
2020-05-06 02:30:04 +0430

    rw

--- index.php
+++ index.php
@@ -20,4 +20,28 @@ $app->get('/messages', function ($request, $response, $args){
     }
     return $response->withStatus(200)->withJson($payload);
 });
+$app->post('/message', function ($request, $response, $args){
+    $_message = $request->getParsedBodyParam('message','');
+    $message = new Message();
+    $message->body = $_message;
+    $message->user_id = -1;
+    $message->save();
+
+    if($message->id){
+        $payload = ['message_id'=>$message->id,
+                    'message_uri'=>'/messages/'.$message->id];
+        return $response->withStatus(201)->withJson($payload);
+    }else{
+        return $response->withStatus(400);
+    }
+});
+$app->delete('/message/{message_id}', function ($request, $response, $args){
+    $message = Message::find($args['message_id']);
+    $message->delete();
+    if($message->exists){
+        return $response->withStatus(400);
+    }else{
+        return $response->withStatus(204);
+    }
+});
 $app->run();
